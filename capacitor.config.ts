a7162767a7b4b904546208427f53ad6ac4bdd85d
wiuwlix2-sudo <wiuwlix2@gmail.com>
2026-01-15 15:43:10 -0600

    Add initial Capacitor configuration file

---
+++ capacitor.config.ts
@@ -0,0 +1,11 @@
+
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.orlando.multitrackmtp',
+  appName: 'MultiTrack MTP',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
